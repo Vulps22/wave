065a67821a23ee75954a4a6c9b05f6f5cbb6e73f
Vulps <ajmcallister5@gmail.com>
2023-05-30 00:26:55 +0100

    Changed Name to Vortex Evader

--- src/com/vulps/main/Game.java
+++ src/com/vulps/main/Game.java
@@ -16,7 +16,7 @@ public class Game extends Canvas implements Runnable {
 
     public Game(){
         handler = new Handler();
-        new Window(WIDTH + 15, HEIGHT + 35, "Wave Defender", this);
+        new Window(WIDTH + 15, HEIGHT + 35, "Vortex Evader", this);
 
         spawner = new Spawn(handler);
         registerHandlers();
